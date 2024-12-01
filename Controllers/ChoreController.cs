e2a2246e31e10f8a7c5cef026d2df9ff5ee26ba8
gunnar ebeling <gunnarebeling@Mac-mini.attlocal.net>
2024-12-01 15:42:53 -0600

    added all handlers for chores

--- Controllers/ChoreController.cs
+++ Controllers/ChoreController.cs
@@ -5,6 +5,7 @@ using HouseRules.Models;
 using HouseRules.Models.DTOs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRules.Controllers;
 [Controller]
@@ -29,7 +30,7 @@ public class ChoreController: ControllerBase
     }
 
     [HttpGet("{id}")]
-    // [Authorize]
+    [Authorize]
     public IActionResult GetDetails(string id)
     {
         ChoreDTO chore = _dbContext.Chores.ProjectTo<ChoreDTO>(_mapper.ConfigurationProvider).Single(c => c.Id == int.Parse(id));
@@ -38,7 +39,7 @@ public class ChoreController: ControllerBase
     }
 
     [HttpPost("{id}/completed")]
-    // [Authorize]
+    [Authorize]
     public IActionResult PostChoreCompletion(string id, string? userId)
     {
         if (userId == null)
@@ -60,7 +61,7 @@ public class ChoreController: ControllerBase
     }
 
     [HttpPost]
-    // [Authorize]
+    [Authorize(Roles="Admin")]
     public IActionResult PostChore(SimpleChoreDTO choreObj)
     {
        
@@ -78,4 +79,91 @@ public class ChoreController: ControllerBase
 
         
     }
+
+    [HttpPut("{id}")]
+    [Authorize(Roles="Admin")]
+    public IActionResult updateChore(SimpleChoreDTO choreObj, string id)
+    {
+        Chore chore =_dbContext.Chores.SingleOrDefault(c => c.Id == int.Parse(id));
+
+        if (chore == null)
+        {
+            return NotFound();   
+        }
+        chore.Name = choreObj.Name;
+        chore.ChoreFrequencyDays = choreObj.ChoreFrequencyDays;
+        chore.Difficulty = choreObj.Difficulty;
+            
+        
+        _dbContext.SaveChanges();
+
+        return NoContent(); 
+    }
+
+    [HttpDelete("{id}")]
+    [Authorize(Roles="Admin")]
+    public IActionResult DeleteChore( string id)
+    {
+        Chore chore =_dbContext.Chores.SingleOrDefault(c => c.Id == int.Parse(id));
+
+        if (chore == null)
+        {
+            return NotFound();   
+        }
+        _dbContext.Chores.Remove(chore);
+        _dbContext.SaveChanges();
+
+        return NoContent(); 
+    }
+
+    [HttpPost("{id}/assign")]
+    [Authorize(Roles="Admin")]
+    public IActionResult AssignUser(string id, string? userId)
+    {
+       
+         Chore chore = _dbContext.Chores.Include(c => c.UserProfiles).SingleOrDefault(c => c.Id == int.Parse(id));
+         UserProfile userProfile = _dbContext.UserProfiles.SingleOrDefault(u => u.Id == int.Parse(userId));
+
+        if (userProfile == null || chore == null)
+        {
+            return BadRequest();
+        }
+        if (chore.UserProfiles.Any(u => u.Id == int.Parse(userId)))
+        {
+            return BadRequest("User is already assigned to this chore.");
+        }
+
+        chore.UserProfiles.Add(userProfile);
+        _dbContext.SaveChanges();
+
+        return Ok();
+
+        
+    }
+
+    [HttpPost("{id}/unassign")]
+    [Authorize(Roles="Admin")]
+    public IActionResult UnassignUser(string id, string? userId)
+    {
+       
+         Chore chore = _dbContext.Chores.Include(c => c.UserProfiles).SingleOrDefault(c => c.Id == int.Parse(id));
+         UserProfile userProfile = _dbContext.UserProfiles.SingleOrDefault(u => u.Id == int.Parse(userId));
+
+        if (userProfile == null || chore == null)
+        {
+            return BadRequest();
+        }
+        if (!chore.UserProfiles.Any(u => u.Id == int.Parse(userId)))
+        {
+            return BadRequest("User is not assigned chore.");
+        }
+
+        chore.UserProfiles.Remove(userProfile);
+        _dbContext.SaveChanges();
+
+        return Ok();
+
+        
+    }
+
 }
